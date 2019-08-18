8143e089e00a18b70c97fb4f1f32928bcb94d0cc
trewanek <trewanek@gmail.com>
2019-08-18 13:56:33 +0900

    Add JWT authentication with RS256, and mod README

--- README.md
+++ README.md
@@ -8,9 +8,30 @@ JWT authentication hands on.
 - [auth0/go-jwt-middleware](https://github.com/auth0/go-jwt-middleware)
 - [dgrijalva/jwt-go](https://github.com/dgrijalva/jwt-go)
 
+## Select Signing Methods and Key Types
+
+- HS256
+- RS256
+
+## Generate RSA private key & public key
+
+```
+# Create private key
+openssl genrsa > demo.rsa
+
+# Create public key
+openssl rsa -pubout < demo.rsa > demo.rsa.pub
+```
+
 ## Run local server: http://localhost:8080
 
-`SIGNINGKEY=YOUR_SIGNINGKEY go run main.go`
+```
+# TYPE:HS256
+JWT_SIGNING_KEY_TYPE=HS256 HS256_SIGNING_KEY=YOUR_SIGNINGKEY go run main.go
+
+# TYPE:RS256
+JWT_SIGNING_KEY_TYPE=RS256 RS256_PRIVATE_SECRET_PATH=YOUR_SECRET_PATH RS256_PUBLIC_PATH=YOUR_PUBLIC_PATH go run main.go
+```
 
 ## How to request to server endpoints
 
@@ -20,9 +41,11 @@ curl -XGET http://localhost:8080/auth
 
 # Request with jwt token to root endpoint example
 curl -XGET http://localhost:8080 -H "Authorization:Bearer {jwt-token}"
+## If work is well, you get bellow message:
+{"code":200,"status":"OK","message":"Hello World"}
 ```
 
-## Referenced below pages or books
+## Referenced below pages or books, thank you
 
 [Go言語で理解するJWT認証 実装ハンズオン](https://qiita.com/po3rin/items/740445d21487dfcb5d9f)
 [jwt.io](https://jwt.io)
--- middleware/auth/auth.go
+++ middleware/auth/auth.go
@@ -1,6 +1,8 @@
 package auth
 
 import (
+	"errors"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"time"
@@ -9,24 +11,97 @@ import (
 	"github.com/dgrijalva/jwt-go"
 )
 
-func GetJwtTokenHandler(w http.ResponseWriter, r *http.Request) {
-	token := jwt.New(jwt.SigningMethodHS256)
-	claims := token.Claims.(jwt.MapClaims)
-	claims["admin"] = true
-	claims["sub"] = "54546557354"
-	claims["name"] = "trewanek"
-	claims["iat"] = time.Now()
-	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
-	tokenString, _ := token.SignedString([]byte(os.Getenv("SIGNINGKEY")))
+const (
+	JWT_SIGNING_KEY_TYPE = "JWT_SIGNING_KEY_TYPE"
+	HS256                = "HS256"
+	RS256                = "RS256"
 
-	_, _ = w.Write([]byte(tokenString))
+	HS256_SIGNING_KEY         = "HS256_SIGNING_KEY"
+	RS256_PRIVATE_SECRET_PATH = "RS256_PRIVATE_SECRET_PATH"
+	RS256_PUBLIC_PATH         = "RS256_PUBLIC_PATH"
+)
+
+func GetJwtTokenHandler(w http.ResponseWriter, r *http.Request) {
+	name := "trewanek"
+	token := ""
+	if os.Getenv(JWT_SIGNING_KEY_TYPE) == HS256 {
+		token, _ = createJwtTokenFromHS256Secret(name)
+	}
+	if os.Getenv(JWT_SIGNING_KEY_TYPE) == RS256 {
+		token, _ = createJwtTokenFromRS256PEM(name)
+	}
+
+	if token == "" {
+		w.WriteHeader(http.StatusInternalServerError)
+		_, _ = w.Write([]byte("token string could not created."))
+		return
+	}
+	_, _ = w.Write([]byte(token))
 }
 
 func JwtMiddleware(handler http.Handler) http.Handler {
+	if os.Getenv(JWT_SIGNING_KEY_TYPE) == HS256 {
+		return jwtMiddlewareHS256(handler)
+	}
+	if os.Getenv(JWT_SIGNING_KEY_TYPE) == RS256 {
+		return jwtMiddlewareRS256(handler)
+	}
+	panic("jwt auth method is not selected.")
+}
+
+func createJwtTokenFromHS256Secret(userName string) (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	setClaims(token, userName)
+	signKey := []byte(os.Getenv(HS256_SIGNING_KEY))
+	return token.SignedString(signKey)
+}
+
+func createJwtTokenFromRS256PEM(userName string) (string, error) {
+	signBytes, err := ioutil.ReadFile(os.Getenv(RS256_PRIVATE_SECRET_PATH))
+	if err != nil {
+		return "", err
+	}
+	signKey, err := jwt.ParseRSAPrivateKeyFromPEM(signBytes)
+	if err != nil {
+		return "", err
+	}
+	token := jwt.New(jwt.SigningMethodRS256)
+	setClaims(token, userName)
+	return token.SignedString(signKey)
+}
+
+func setClaims(token *jwt.Token, userName string) jwt.MapClaims {
+	claims := token.Claims.(jwt.MapClaims)
+	claims["admin"] = true
+	claims["sub"] = "54546557354"
+	claims["name"] = userName
+	claims["iat"] = time.Now()
+	claims["exp"] = time.Now().Add(time.Hour * 24).Unix()
+	return claims
+}
+
+func jwtMiddlewareHS256(handler http.Handler) http.Handler {
 	return jwtmiddleware.New(jwtmiddleware.Options{
 		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
-			return []byte(os.Getenv("SIGNINGKEY")), nil
+			return []byte(os.Getenv(HS256_SIGNING_KEY)), nil
 		},
 		SigningMethod: jwt.SigningMethodHS256,
+	}).Handler(handler)
+}
+
+func jwtMiddlewareRS256(handler http.Handler) http.Handler {
+	return jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			b, err := ioutil.ReadFile(os.Getenv(RS256_PUBLIC_PATH))
+			if err != nil {
+				return nil, err
+			}
+			signKey, err := jwt.ParseRSAPublicKeyFromPEM(b)
+			if err != nil {
+				return nil, errors.New("private key could not be parsed")
+			}
+			return signKey, nil
+		},
+		SigningMethod: jwt.SigningMethodRS256,
 	}).Handler(handler)
 }
